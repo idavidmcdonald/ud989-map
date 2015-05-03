a560a3dae68abe7a4711470871bab75dd9940a86
David McDonald <idavidmcdonald@me.com>
2015-05-03 15:11:47 +0100

    added reviews for burgers

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ var burgerData = [
 		price: 9,
 		includesChips: true,
 		lat: 51.541374,
-		lng: -0.146492
+		lng: -0.146492,
+		review: "Good sized cheeseburger, brioche bun, with your choice of cheddar, red leicster or stilton - I went for the cheddar. Comes with a super tasty red onion relish. Mostly importantly, the meat has been the juiciest and best seasoned, literally driblling with juices. Comes with their rosemary salt fries as default. I've been a few times and Honest Burger will give you a consistently great burger, there isn't really anything about the burger not up to scratch."
 	},
 	{
 		name: 'Wild Game Co.',
@@ -15,7 +16,8 @@ var burgerData = [
 		price: 6,
 		includesChips: false,
 		lat: 51.523047,
-		lng: -0.092878
+		lng: -0.092878,
+		review: "First thing to note is that this is a venison burger (no beef on the menu here). Served in a brioche bun (although maybe a tad too sweet) with crisp lettuce, one single roast cherry tomato and their homemade buffalo style sauce. I've been a few times and the meat has been excellent and juicy, definitely setting it apart from many others, however I'm not convinced by their choice of toppings, bringing this one down a point."
 	},
 	{
 		name: 'Byron Burger',
@@ -24,7 +26,8 @@ var burgerData = [
 		price: 7.95,
 		includesChips: false,
 		lat: 51.513037,
-		lng: -0.133858
+		lng: -0.133858,
+		review: "A standard cheeseburger - nothing wrong with it but nothing setting it apart from the rest (apart from the inclusion of a tasty pickle on the side). "
 	},
 	{
 		name: 'Dirty Burger',
@@ -33,7 +36,8 @@ var burgerData = [
 		price: 6,
 		includesChips: false,
 		lat: 51.554122,
-		lng: -0.145158
+		lng: -0.145158,
+		review: "Hidden behind a building in a shack (literally), the place is a little hard to find but the burger is pretty good. Comes with lots of melted mozerella and is somewhat dirty to eat as expected but nothing too much to write home about. "
 	},
 	{
 		name: 'Shake Shack',
@@ -42,7 +46,8 @@ var burgerData = [
 		price: 5,
 		includesChips: false,
 		lat: 51.511507,
-		lng: -0.122751
+		lng: -0.122751,
+		review: "Small american style burger, similar to In and Out however not as good. Burger pattys are thin so you won't be getting anything medium rare. It's definitely a good step up from McDonalds however you can find burgers twice the size and twice as tasty for only a pound or two more."
 	},
 	{
 		name: 'Patty and Bun',
@@ -51,7 +56,8 @@ var burgerData = [
 		price: 7.5,
 		includesChips: false,
 		lat: 51.517349,
-		lng: -0.082100
+		lng: -0.082100,
+		review: "One seriously attractive looking burger, a good size too. Comes with their smokey bbq mayo which I'm not fully convinced is better then classic ketchup. The only thing letting this down a tad is the meat - it's pretty good and came perfectly cooked, however it wasn't juicy or seasoned enough to get into the top ratings. This place does a great burger and definitely has a lot of potential - worth a visit.  "
 	},
 	{
 		name: '5 Guys',
@@ -60,7 +66,8 @@ var burgerData = [
 		price: 8,
 		includesChips: false,
 		lat: 51.511890, 
-		lng: -0.126894
+		lng: -0.126894,
+		review: "£8? They must be having a laugh. You're paying for the hype and cool american branding. Unlike most others, you can pick from about 15 free toppings to have on your burger (think variety of sauces, pickles, salad etc), which is nice if you want to cram them all on and get your moneys worth. The burger is american styled and the pattys are thin so no pink meat. It's not a bad burger, fairly tasty and greasy, but just not worth the price."
 	},
 	{
 		name: 'Stokey Bears',
@@ -69,7 +76,8 @@ var burgerData = [
 		price: 7.5,
 		includesChips: false,
 		lat: 51.560508, 
-		lng: -0.074084
+		lng: -0.074084,
+		review: "A very tasty good sized burger. Maybe not as fancy as places such as Patty n Bun but worth a visit. Comes with lettuce and a good bit of raw onion for sweetness. The burger came just a smidgen overcooked but the meat was still good, although not up the quality of somewhere like Honest Burger. This place has good potential and if the patty got a little better then it could definitely be pushing up the rankings."
 	},
 	{
 		name: 'Hache',
@@ -90,7 +98,8 @@ var burgerData = [
 		price: 6,
 		includesChips: false,
 		lat: 51.519891, 
-		lng: -0.075201
+		lng: -0.075201,
+		review: "Maybe I'm just unlucky, but the two burgers I've heard from Bleeker Street, the first was quite undercooked and the second was well overcooked. They haven't gone for a brioche bun and instead use a normal toasted roll. All in all, it reminds me of a fancy Big Mac but lacking the lettuce and sauce so it's a little dry. Maybe I haven't had a proper burger from there but so far I'm not sure what the good reviews are all about."
 	},
 	{
 		name: 'Gourmet Burger Kitchen',
@@ -163,6 +172,7 @@ var Burger = function(data){
 	this.includesChips = ko.observable(data.includesChips);
 	this.lat = ko.observable(data.lat);
 	this.lng = ko.observable(data.lng);
+	this.review = ko.observable(data.review);
 	//this.current = ko.observable(false);
 	this.visible = ko.observable(true);
 };
